18783077885693a42fe772af5804df8a6dd0aebc
theAmirKhalili <gobagm2h@gmail.com>
2021-11-28 03:54:14 +0330

    Some animation added, dots added to slideshow

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,19 @@
 var myIndex = 0;
-carousel();
+slideshow();
 
-function carousel() {
+function slideshow() {
   var i;
   var x = document.getElementsByClassName("slides");
+  var dots = document.getElementsByClassName("dot");
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";  
   }
   myIndex++;
   if (myIndex > x.length) {myIndex = 1}    
-  x[myIndex-1].style.display = "grid";
-  setTimeout(carousel, 3500);    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  x[myIndex-1].style.display = "grid";  
+  dots[myIndex-1].className += " active";
+  setTimeout(slideshow, 3500);
 }
-
